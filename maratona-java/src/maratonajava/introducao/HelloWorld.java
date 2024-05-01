298b296a3cc44f9ccf2af66e7a9cbf73fa1d242f
Paulo Roberto <paulorobertontm@gmail.com>
2024-05-01 20:11:48 -0300

    Primeira aula.

--- maratona-java/src/maratonajava/introducao/HelloWorld.java
+++ maratona-java/src/maratonajava/introducao/HelloWorld.java
@@ -2,6 +2,6 @@ package maratonajava.introducao;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!");
     }
 }
