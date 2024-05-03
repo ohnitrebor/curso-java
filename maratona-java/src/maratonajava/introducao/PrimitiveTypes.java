6b40671cae39be780bf081ef2748d5410676d3e1
Paulo Roberto <paulorobertontm@gmail.com>
2024-05-02 21:08:00 -0300

    Revisei e atualizei o código.

--- maratona-java/src/maratonajava/introducao/PrimitiveTypes.java
+++ maratona-java/src/maratonajava/introducao/PrimitiveTypes.java
@@ -4,10 +4,10 @@ public class PrimitiveTypes {
     public static void main(String[] args) {
         int age = 10;
         long bigNumber = 100000L;
-        double salaryDouble = 2000.0D;
-        float salaryFloat = 2500.0F;
-        byte ageByte = -128;
-        short ageShort = 32000;
+        double salaryOne = 2000.0D;
+        float salaryTwo = 2500.0F;
+        byte ageOne = -128;
+        short ageTwo = 32000;
         boolean tru = true;
         boolean fake = false;
         char characters = 'M';
@@ -15,8 +15,13 @@ public class PrimitiveTypes {
 
         System.out.println("Age is "+age+" years.");
         System.out.println(fake);
+        System.out.println(tru);
         System.out.println("Gender: "+characters);
         System.out.println(bigNumber);
         System.out.println("Hi, my name is "+name+"!");
+        System.out.println(salaryOne);
+        System.out.println(salaryTwo);
+        System.out.println(ageOne);
+        System.out.println(ageTwo);
     }
 }
